227ca8ce93e1099f443e10a3bc9ff42e8cb48f6b
Morgan McDermott <morganmcdermott@Morgans-MacBook-Pro.local>
2019-09-01 15:17:06 -0700

    Added batching functionality

--- pressurize/api/batch.go
+++ pressurize/api/batch.go
@@ -24,7 +24,7 @@ func MethodCanBatch(model string, method string) bool {
 	model_config := models[model]
 	for _, m := range model_config.Methods {
 		if m == "batch_" + method {
-			return true
+			return false
 		}
 	}
 	return true
